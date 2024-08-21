5853cfca1ea7ce6f396b60a8a3ac1a5de2d3141c
ninetailsrabbit <danieldigitalspace@proton.me>
2024-08-21 09:56:13 +0100

    created basic folders for a starting point

---
+++ autoload/game_globals.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+const world_collision_layer: int = 0
+const player_collision_layer: int = 2
+const enemies_collision_layer: int = 4
+const interactables_collision_layer: int = 8
+const throwables_collision_layer: int = 16
+const hitboxes_collision_layer: int = 32
+
+var next_scene_path: String = ""
