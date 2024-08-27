98573bc2abb20986af1cfc310394e88e944468c9
ninetailsrabbit <danieldigitalspace@proton.me>
2024-08-27 09:57:36 +0100

    camera 2d changed some variable names

--- utilities/viewport/camera_2d_helper.gd
+++ utilities/viewport/camera_2d_helper.gd
@@ -6,8 +6,8 @@ func get_panning_camera_position(camera: Camera2D) -> Vector2:
 	
 	var current_mouse_screen_position = viewport.get_mouse_position()
 	var original_mouse_global_position = (viewport.get_screen_transform() * viewport.get_canvas_transform()).affine_inverse() * viewport.get_mouse_position()
-	var final_mouse_global_pos = (viewport.get_screen_transform() * viewport.get_canvas_transform()).affine_inverse() * current_mouse_screen_position
-	var mouse_global_diff = original_mouse_global_position - final_mouse_global_pos
+	var final_mouse_global_position = (viewport.get_screen_transform() * viewport.get_canvas_transform()).affine_inverse() * current_mouse_screen_position
+	var mouse_global_diff = original_mouse_global_position - final_mouse_global_position
 	var new_camera_position = camera.global_position + mouse_global_diff
 	
 	if new_camera_position != camera.global_position:
