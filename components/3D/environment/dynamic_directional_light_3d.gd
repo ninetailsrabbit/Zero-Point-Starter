7d9df88ad33e4c231b8a47e7181cb162431514a5
ninetailsrabbit <danieldigitalspace@proton.me>
2024-09-26 08:38:21 +0100

    fixed a typo accessing the graphic quality settings on dynamic world nodes

--- components/3D/environment/dynamic_directional_light_3d.gd
+++ components/3D/environment/dynamic_directional_light_3d.gd
@@ -13,17 +13,17 @@ func on_updated_graphic_settings() -> void:
 
 func update_world_graphic_settings() -> void:
 	var quality_preset = SettingsManager.get_graphics_section("quality_preset")
-	
-	for preset in HardwareDetector.graphics_quality_presets[quality_preset]:
-		for quality in preset.quality:
-			match quality.project_setting:
-				"shadow_atlas":
-					match quality_preset:
-						HardwareDetector.QualityPreset.Low:
-							shadow_bias = 0.03
-						HardwareDetector.QualityPreset.Medium:
-							shadow_bias = 0.02
-						HardwareDetector.QualityPreset.High:
-							shadow_bias = 0.01
-						HardwareDetector.QualityPreset.Ultra:
-							shadow_bias = 0.005
+	var preset: HardwareDetector.GraphicQualityPreset = HardwareDetector.graphics_quality_presets[quality_preset]
+
+	for quality: HardwareDetector.GraphicQualityDisplay in preset.quality:
+		match quality.project_setting:
+			"shadow_atlas":
+				match quality_preset:
+					HardwareDetector.QualityPreset.Low:
+						shadow_bias = 0.03
+					HardwareDetector.QualityPreset.Medium:
+						shadow_bias = 0.02
+					HardwareDetector.QualityPreset.High:
+						shadow_bias = 0.01
+					HardwareDetector.QualityPreset.Ultra:
+						shadow_bias = 0.005
